bdf86d67dead9c228242f76c10dc260f8f5ac33c
Daniel Aldemir <daniel.aldemir79@gmail.com>
2025-09-15 18:49:12 +0200

    Metoder skapade

--- Program.cs
+++ Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-           
+
+       
+        
+        
+        
+        
+        
+        
+        }
+
+        static double CalculateBMI(double weight, double height, string unit = "metric")
+        {
+            if (unit == "metric")
+            {
+                return weight / (height * height);
+            }
+            else if (unit == "imperial")
+            {
+                return 703 * (weight / (height * height));
+            }
+            else
+            {
+                Console.WriteLine("Okänd enhet, returnerar 0");
+                return 0;
+            }
+        }
+
+        static int GetInt()
+        {
+            int helTal;
+
+            while (int.TryParse(Console.ReadLine(), out helTal) == false)
+            {
+                Console.WriteLine("Du har angett felaktigt värde. Försök igen.");
+            }
+
+            return helTal;
+
+        }
+        static double GetDouble()
+        {
+            double decimalTal;
+
+            while (double.TryParse(Console.ReadLine(), out decimalTal) == false)
+            {
+                Console.WriteLine("Du har angett felaktigt värde. Försök igen.");
+            }
+
+            return decimalTal;
+
         }
     }
 }
